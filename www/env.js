ccfb8dac560ea039048cdf55047407eee29a06e2
AdamRB208 <adamrborup@gmail.com>
2025-03-11 14:24:55 -0600

    created houses schema and added data.

--- www/env.js
+++ www/env.js
@@ -1,3 +1,6 @@
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+NODE_ENV = dev
+PORT = 3000
+CONNECTION_STRING = mongodb + srv://AdamRB:a7Fx^zP5%+z5@EH@db2571.j2deo.mongodb.net/
+AUTH_DOMAIN =
+  AUTH_AUDIENCE =
+  AUTH_CLIENT_ID =
