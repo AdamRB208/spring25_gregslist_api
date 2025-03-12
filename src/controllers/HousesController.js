291de83c35157102100b7304e6e03775c97c194a
AdamRB208 <adamrborup@gmail.com>
2025-03-12 00:49:30 -0600

    defeat

--- src/controllers/HousesController.js
+++ src/controllers/HousesController.js
@@ -7,6 +7,7 @@ export class HousesController extends BaseController {
     super('api/houses')
     this.router
       .get('', this.getHouses)
+      .get('/year', this.getHouseByQuery)
       .get('/:houseId', this.getHouseById)
 
 
@@ -41,6 +42,23 @@ export class HousesController extends BaseController {
       const houseId = request.params.houseId
       const house = await housesService.getHouseById(houseId)
       response.send(house)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+    * @param {import("express").Request} request
+    * @param {import("express").Response} response
+    * @param {import("express").NextFunction} next
+    */
+
+
+  async getHouseByQuery(request, response, next) {
+    try {
+      const house = await housesService.getHouseByQuery()
+      response.send(house)
+      console.log('this is running')
     } catch (error) {
       next(error)
     }
--- src/services/HousesService.js
+++ src/services/HousesService.js
@@ -3,17 +3,23 @@ import { dbContext } from "../db/DbContext.js";
 
 
 class HousesService {
+
   async getHouses() {
     const houses = await dbContext.Houses.find().populate('creator')
     return houses
   }
 
   async getHouseById(houseId) {
-    const house = await (await dbContext.Houses.findById(houseId)).populated('creator')
+    const house = await (await dbContext.Houses.findById(houseId)).populate('creator')
     // console.log('found house', house);
     if (house == null) {
       throw new BadRequest(`${houseId} is not a valid house id!`)
     }
+    return house
+  }
+
+  async getHouseByQuery() {
+    const house = await dbContext.Houses.find({ year: 2022 }).populate('creator')
     return house
   }
 
